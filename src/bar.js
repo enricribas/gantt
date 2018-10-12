e24b7a055bc22156cb9c66479673a687966196d6
Enric Ribas <ribas.enric@gmail.com>
2018-10-12 07:06:32 -0400

    Allow for not opening popup and having custom action

--- src/bar.js
+++ src/bar.js
@@ -32,8 +32,8 @@ export default class Bar {
         this.width = this.gantt.options.column_width * this.duration;
         this.progress_width =
             this.gantt.options.column_width *
-                this.duration *
-                (this.task.progress / 100) || 0;
+            this.duration *
+            (this.task.progress / 100) || 0;
         this.group = createSVG('g', {
             class: 'bar-wrapper ' + (this.task.custom_class || ''),
             'data-id': this.task.id
@@ -49,19 +49,19 @@ export default class Bar {
     }
 
     prepare_helpers() {
-        SVGElement.prototype.getX = function() {
+        SVGElement.prototype.getX = function () {
             return +this.getAttribute('x');
         };
-        SVGElement.prototype.getY = function() {
+        SVGElement.prototype.getY = function () {
             return +this.getAttribute('y');
         };
-        SVGElement.prototype.getWidth = function() {
+        SVGElement.prototype.getWidth = function () {
             return +this.getAttribute('width');
         };
-        SVGElement.prototype.getHeight = function() {
+        SVGElement.prototype.getHeight = function () {
             return +this.getAttribute('height');
         };
-        SVGElement.prototype.getEndX = function() {
+        SVGElement.prototype.getEndX = function () {
             return this.getX() + this.getWidth();
         };
     }
@@ -185,10 +185,16 @@ export default class Bar {
                 this.gantt.trigger_event('click', [this.task]);
             }
 
+            if (e.type === 'dblclick') {
+                this.gantt.trigger_event('dblclick', [this.task]);
+            }
+
             this.gantt.unselect_all();
             this.group.classList.toggle('active');
 
-            this.show_popup();
+            if (this.gantt.options.show_popup) {
+                this.show_popup();
+            }
         });
     }
 
